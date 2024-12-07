4aedb737a280440c0198738ca02600714b8d32c2
soul_mask <bprabhanjen@gmail.com>
2024-12-07 19:50:19 +0530

    Create Offensive_security.sh

---
+++ Cyber security 101/Offensive_security.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#first create a text file and a script file in the same file
+touch result.txt
+touch script.sh
+
+#edit the script.sh using nano or vim
+nano script.sh
+
+#type this in the script :
+#   #!/bin/bash
+#   echo"$(gobuster -u http://fakebank.thm -w wordlist.txt)" > result.txt
+
+# the whole gobuster result will be stored in the file result.txt
+
+# we can see /bank-transfer with the status code 200 which is our necessary result
+
+#then use below  command to open the website through terminal 
+xdg-open http://fakebank.thm/bank-transfer
+
+#then according to the ask transfer the amount to your account
